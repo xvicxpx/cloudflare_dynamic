cb5c1f1b9670a2139d165376843d0b7f1e4db451
Vic Chan <xvicxpx@gmail.com>
2022-05-31 21:45:13 -0400

    Fixed Docker Compose File To Remove Token Information and added more status logging

--- update.py
+++ update.py
@@ -12,16 +12,21 @@ def get_ip():
 def main():
     zone = Zone()
     zone_results = zone.get_zones(name="fleetfarm.us")
-    zone_id = zone_results["r\esult"][0]["id"]
+    zone_id = zone_results["result"][0]["id"]
 
     dns = DNS(zone_id)
 
     dns_first_result = dns.list_dns_records(name="fleetfarm.us", type="A")["result"][0]
 
+    print(f"Current IP is: {get_ip()}")
+
     # Checks if needs to even update the DNS record
     if dns_first_result["content"] == get_ip():
+        print("IP Address Does Not Need To Be Updated")
         return
     else:
+        print(f'IP Address Updated From {dns_first_result["content"]} to {get_ip()}')
+
         dns_identifier = dns_first_result["id"]
 
         dns.update_dns_record(
@@ -33,6 +38,9 @@ def main():
             proxied=True,
         )
 
+        print("Updating Completed")
+
 
 if __name__ == "__main__":
+    print("Updating DNS address")
     main()
